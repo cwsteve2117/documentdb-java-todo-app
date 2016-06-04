ba069acf8f8d3f9f7d1699091084ba1839d14663
Colin Stevenson <cstevenson@Colin-Stevenson-MacBook-Pro.local>
2016-06-04 09:19:05 -0400

    cleaned up printlns and comments

--- src/com/microsoft/azure/documentdb/sample/dao/DocumentClientFactory.java
+++ src/com/microsoft/azure/documentdb/sample/dao/DocumentClientFactory.java
@@ -10,15 +10,6 @@ import com.microsoft.azure.documentdb.sample.model.DocDbCredentials;
 
 public class DocumentClientFactory {
     
-    /*
-    private CloudFactory cloudFactory = new CloudFactory();
-    private Cloud cloud = cloudFactory.getCloud();
-    // ServiceInfo has all the information necessary to connect to the underlying service
-    List<ServiceInfo> serviceInfos = cloud.getServiceInfos();
-    // Find the `ServiceInfo` definitions suitable for connecting to a particular service type
-    List<ServiceInfo> databaseInfos = cloud.getServiceInfos(Database.class);
-	*/
-    
     private static DocumentClient documentClient;
     private static DocDbCredentials docDbCredentials = new DocDbCredentials();
 
@@ -30,24 +21,17 @@ public class DocumentClientFactory {
     			ObjectMapper mapper = new ObjectMapper();
     		    JsonParser jp = mapper.getFactory().createParser(vcap_services);
     		    JsonNode rootNode = mapper.readTree(jp);
-    		    JsonNode documentDbNode = rootNode.get("documentdb");
+    		    JsonNode documentDbNode = rootNode.get("cs-documentdb");
     		    if(documentDbNode != null){
-    		    	System.out.println("brokered service");
     		    	JsonNode credentials = documentDbNode.get(0).get("credentials");
     		    	docDbCredentials.setHost(credentials.get("documentdb_host").asText());
     		    	docDbCredentials.setMasterKey(credentials.get("documentdb_key").asText());
     		    } else if(rootNode.get("user-provided") != null){
-     		    	System.out.println("user provided service");
      		    	documentDbNode = rootNode.get("user-provided");
      		    	JsonNode credentials = documentDbNode.get(0).get("credentials");
-     		    	System.out.print("credentials: " + credentials.toString());
      		    	docDbCredentials.setHost(credentials.get("documentdb_host").asText());
-     		    	System.out.print("host: " + docDbCredentials.getHost());
     		    	docDbCredentials.setMasterKey(credentials.get("documentdb_key").asText()); 
-    		    	System.out.print("master key: " + docDbCredentials.getMasterKey());
     		    }
-    		    System.out.println(docDbCredentials.getHost());
-    		    System.out.println(docDbCredentials.getMasterKey());
     		    
     		    if (documentClient == null) {
                     documentClient = new DocumentClient(docDbCredentials.getHost(), docDbCredentials.getMasterKey(),
@@ -72,16 +56,14 @@ public class DocumentClientFactory {
     			ObjectMapper mapper = new ObjectMapper();
     		    JsonParser jp = mapper.getFactory().createParser(vcap_services);
     		    JsonNode rootNode = mapper.readTree(jp);
-    		    JsonNode documentDbNode = rootNode.get("documentdb");
+    		    JsonNode documentDbNode = rootNode.get("cs-documentdb");
     		    if(documentDbNode != null){
-    		    	System.out.println("brokered service");
     		    	JsonNode credentials = documentDbNode.get(0).get("credentials");
     		    	docDbCredentials.setHost(credentials.get("documentdb_host").asText());
     		    	docDbCredentials.setMasterKey(credentials.get("documentdb_key").asText());
     		    	docDbCredentials.setDocumentDbName(credentials.get("documentdb_database").asText());
     		    	docDbCredentials.setDocumentDbResourceId(credentials.get("documentdb_resource_id").asText());
     		    } else if(rootNode.get("user-provided") != null){
-     		    	System.out.println("user provided service");
      		    	documentDbNode = rootNode.get("user-provided");
      		    	JsonNode credentials = documentDbNode.get(0).get("credentials");
      		    	docDbCredentials.setHost(credentials.get("documentdb_host").asText());
@@ -89,10 +71,6 @@ public class DocumentClientFactory {
     		    	docDbCredentials.setDocumentDbName(credentials.get("documentdb_database").asText());
     		    	docDbCredentials.setDocumentDbResourceId(credentials.get("documentdb_resource_id").asText());  		    	
     		    }
-    		    System.out.println(docDbCredentials.getHost());
-    		    System.out.println(docDbCredentials.getMasterKey());
-    		    System.out.println(docDbCredentials.getDocumentDbName());
-    		    System.out.println(docDbCredentials.getDocumentDbResourceId());
     		    
     		    if (documentClient == null) {
                     documentClient = new DocumentClient(docDbCredentials.getHost(), docDbCredentials.getMasterKey(),
